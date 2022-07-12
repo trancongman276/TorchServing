3700b00a769b80c93b4a20b68e7ecd6a527d5188
trancongman276 <trancongman276@gmail.com>
2022-07-12 15:15:15 +0700

    add model into entrypoint

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [[ "$1" = "serve" ]]; then
     shift 1
-    torchserve --start --ts-config /home/model-server/config.properties
+    torchserve --start --ts-config /home/model-server/config.properties --models /home/model-server/model-store/atr.mar
 else
     eval "$@"
 fi
